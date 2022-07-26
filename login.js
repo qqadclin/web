28931e238d21a67ce6e2d56464fea19f8e08075a
qqadclin <qqadclin@163.com>
2022-07-26 14:06:13 +0900

    first commit

---
+++ login.js
@@ -0,0 +1,9 @@
+function login() {
+    var pass = document.getElementById("password");
+
+    if (pass.Value == password) {
+        window.location.href = "kimetu.html" ;
+    } else {
+       window.location.href = "kimetu.html";
+    }
+}
